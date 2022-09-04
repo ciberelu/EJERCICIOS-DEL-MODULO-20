b96fcb61e072e61e5b0247a9662ff702ef120a24
ciberelu <ciberelu@gmail.com>
2022-09-04 15:35:43 -0600

    first commit

---
+++ ejercicio_1.py
@@ -0,0 +1,18 @@
+
+
+from posixpath import split
+
+
+paises_usuario = input("ingrese los paises separados por coma ")
+paises_usuario = paises_usuario.split(", ")
+sinRepetidos = []
+
+for elemento in paises_usuario:
+    if elemento not in sinRepetidos:
+        
+        sinRepetidos.append(elemento)
+
+ordenado = sorted(sinRepetidos)
+print(ordenado)
+
+
